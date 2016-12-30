873b07160f48149f6b476c301c6642168fed9ba9
moth <moth@gionee.com>
2016-12-30 16:36:37 +0800

    init project

---
+++ app/src/main/java/com/smona/app/detect/facedetector/MainActivity.java
@@ -0,0 +1,290 @@
+package com.smona.app.detect.facedetector;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.support.design.widget.Snackbar;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.SparseArray;
+
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+import com.google.android.gms.vision.Frame;
+
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Bitmap mImage = null;
+    private Toast mToast;
+    private File mPictureFile;
+    private FaceDetector mDetector;
+    private Detector<Face> mSafeDetector;
+    private FaceView mFaceView;
+
+    private static final int RC_HANDLE_GMS = 9001;
+    // permission request codes need to be < 256
+    private static final int RC_HANDLE_CAMERA_PERM = 2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.offline_pick).setOnClickListener(MainActivity.this);
+        findViewById(R.id.offline_camera).setOnClickListener(MainActivity.this);
+
+
+        mFaceView = (FaceView) findViewById(R.id.faceOverlay);
+        mToast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+
+        // Check for the camera permission before accessing the camera.  If the
+        // permission is not granted yet, request permission.
+        int rc = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (rc == PackageManager.PERMISSION_GRANTED) {
+            initFaceDetector();
+        } else {
+            requestCameraPermission();
+        }
+
+    }
+
+    private void initFaceDetector() {
+        mDetector = new FaceDetector.Builder(getApplicationContext())
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .build();
+
+        // This is a temporary workaround for a bug in the face detector with respect to operating
+        // on very small images.  This will be fixed in a future release.  But in the near term, use
+        // of the SafeFaceDetector class will patch the issue.
+        mSafeDetector = new SafeFaceDetector(mDetector);
+    }
+
+    private void requestCameraPermission() {
+        Log.w(TAG, "Camera permission is not granted. Requesting permission");
+
+        final String[] permissions = new String[]{Manifest.permission.CAMERA};
+
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            ActivityCompat.requestPermissions(this, permissions, RC_HANDLE_CAMERA_PERM);
+            return;
+        }
+
+        final Activity thisActivity = this;
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ActivityCompat.requestPermissions(thisActivity, permissions,
+                        RC_HANDLE_CAMERA_PERM);
+            }
+        };
+
+        Snackbar.make(mFaceView, R.string.permission_camera_rationale,
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.ok, listener)
+                .show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.offline_pick:
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_PICK);
+                startActivityForResult(intent, FaceUtil.REQUEST_PICTURE_CHOOSE);
+                break;
+            case R.id.offline_camera:
+                // 设置相机拍照后照片保存路径
+                mPictureFile = new File(Environment.getExternalStorageDirectory(),
+                        "picture" + System.currentTimeMillis() / 1000 + ".jpg");
+                // 启动拍照,并保存到临时文件
+                Intent mIntent = new Intent();
+                mIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                mIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mPictureFile));
+                mIntent.putExtra(MediaStore.Images.Media.ORIENTATION, 0);
+                startActivityForResult(mIntent, FaceUtil.REQUEST_CAMERA_IMAGE);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void detectorFace() {
+        if (null != mImage) {
+            //处理图片
+            Log.d("moth", "mDetector.isOperational()=" + mDetector.isOperational());
+            if (!mDetector.isOperational()) {
+                //Handle contingency
+            } else {
+                Frame frame = new Frame.Builder().setBitmap(mImage).build();
+                SparseArray<Face> faces = mSafeDetector.detect(frame);
+                Log.d("moth", "mSafeDetector.isOperational()=" + mSafeDetector.isOperational());
+                if (!mSafeDetector.isOperational()) {
+                    // Note: The first time that an app using face API is installed on a device, GMS will
+                    // download a native library to the device in order to do detection.  Usually this
+                    // completes before the app is run for the first time.  But if that download has not yet
+                    // completed, then the above call will not detect any faces.
+                    //
+                    // isOperational() can be used to check if the required native library is currently
+                    // available.  The detector will automatically become operational once the library
+                    // download completes on device.
+                    Log.w(TAG, "Face detector dependencies are not yet available.");
+
+                    // Check for low storage.  If there is low storage, the native library will not be
+                    // downloaded, so detection will not become operational.
+                    IntentFilter lowstorageFilter = new IntentFilter(Intent.ACTION_DEVICE_STORAGE_LOW);
+                    boolean hasLowStorage = registerReceiver(null, lowstorageFilter) != null;
+
+                    if (hasLowStorage) {
+                        Toast.makeText(this, R.string.low_storage_error, Toast.LENGTH_LONG).show();
+                        Log.w(TAG, getString(R.string.low_storage_error));
+                    }
+                }
+                mFaceView.setContent(mImage, faces);
+
+                // Although detector may be used multiple times for different images, it should be released
+                // when it is no longer needed in order to free native resources.
+            }
+        } else {
+            showTip("请选择图片后再检测");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        String fileSrc = null;
+        if (requestCode == FaceUtil.REQUEST_PICTURE_CHOOSE) {
+            if ("file".equals(data.getData().getScheme())) {
+                // 有些低版本机型返回的Uri模式为file
+                fileSrc = data.getData().getPath();
+            } else {
+                // Uri模型为content
+                String[] proj = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(data.getData(), proj,
+                        null, null, null);
+                cursor.moveToFirst();
+                int idx = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                fileSrc = cursor.getString(idx);
+                cursor.close();
+            }
+        } else if (requestCode == FaceUtil.REQUEST_CAMERA_IMAGE) {
+            if (null == mPictureFile) {
+                showTip("拍照失败，请重试");
+                return;
+            }
+
+            fileSrc = mPictureFile.getAbsolutePath();
+            updateGallery(fileSrc);
+        }
+
+        if (fileSrc != null) {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            mImage = BitmapFactory.decodeFile(fileSrc, options);
+
+            // 适当压缩图片可以加快检测速度
+            options.inSampleSize = Math.max(1, (int) Math.ceil(Math.max(
+                    (double) options.outWidth / 1024f,
+                    (double) options.outHeight / 1024f)));
+            options.inJustDecodeBounds = false;
+            mImage = BitmapFactory.decodeFile(fileSrc, options);
+
+            // 部分手机会对图片做旋转，这里检测旋转角度
+            int degree = FaceUtil.readPictureDegree(fileSrc);
+            if (degree != 0) {
+                // 把图片旋转为正的方向
+                mImage = FaceUtil.rotateImage(degree, mImage);
+            }
+            detectorFace();
+            // ((ImageView) findViewById(R.id.offline_img)).setImageBitmap(mImage);
+            // 清除上次人脸检测结果
+        }
+
+    }
+
+    private void updateGallery(String filename) {
+        MediaScannerConnection.scanFile(this, new String[]{filename}, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+
+                    @Override
+                    public void onScanCompleted(String path, Uri uri) {
+
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        // 销毁对象
+        mSafeDetector.release();
+        super.onDestroy();
+    }
+
+
+    private void showTip(final String str) {
+        mToast.setText(str);
+        mToast.show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode != RC_HANDLE_CAMERA_PERM) {
+            Log.d(TAG, "Got unexpected permission result: " + requestCode);
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            return;
+        }
+
+        if (grantResults.length != 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Camera permission granted - initialize the camera source");
+            // we have permission, so create the camerasource
+            return;
+        }
+
+        Log.e(TAG, "Permission not granted: results len = " + grantResults.length +
+                " Result code = " + (grantResults.length > 0 ? grantResults[0] : "(empty)"));
+
+        DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Face Tracker sample")
+                .setMessage(R.string.no_camera_permission)
+                .setPositiveButton(R.string.ok, listener)
+                .show();
+    }
+}
